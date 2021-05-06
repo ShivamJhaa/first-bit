b3018aa8cc43076b00063ddcaa0064a75e69695b
sssvjha <sssvjha@gmail.com>
2021-05-06 13:36:18 +0530

    new

---
+++ catalyn.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Calculating the catalyn series
+
+int dp[1000]={0};
+
+int Catalyn(int n)
+{
+    if (n==0)
+    return 1;
+
+    if (dp[n]!=0)
+    {
+        return dp[n];
+    }
+
+    int ans=0;
+    for (int i=1;i<=n;i++)
+    {
+        ans+=Catalyn(i-1)*Catalyn(n-i);
+    }
+
+    dp[n]=ans;
+
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i=0;i<=n;i++){
+        cout<<Catalyn(i)<<" ,";
+    }
+}
